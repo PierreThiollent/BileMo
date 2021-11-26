398bd050cfbd33999b861a902338ae634bb403cb
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-11-26 19:58:49 +0100

    Add Doctrine, Security Bundle and Lexik JWT authentication bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
