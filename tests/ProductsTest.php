b101275731e9377581aa72a980ce61c6f4efcedd
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-12-10 10:10:33 +0100

    Update product test : add new method to setUp client. Add method to test get products without JWT. Update getAllProducts because of JSON LD data. Add new method to check that the response contains JSON LD data

--- tests/ProductsTest.php
+++ tests/ProductsTest.php
@@ -3,73 +3,111 @@
 namespace App\Tests;
 
 use App\Entity\Product;
+use JMS\Serializer\Serializer;
+use JsonException;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductsTest extends WebTestCase
 {
-    /**
-     * @throws \JsonException
-     */
-    private function createAuthenticatedClient(): KernelBrowser
+    private KernelBrowser $client;
+    private Serializer $serializer;
+
+    public function testGetAllProductsWithoutJWTShouldReturn401(): void
     {
-        $client = static::createClient();
-        $client->request(
-            'POST',
-            '/api/login',
-            [],
-            [],
-            ['CONTENT_TYPE' => 'application/json'],
-            json_encode(
-                ['email' => 'pierre.thiollent76@gmail.com', 'password' => 'test'],
-                JSON_THROW_ON_ERROR
-            )
-        );
+        $this->client->request('GET', '/api/products');
+        self::assertResponseStatusCodeSame(401);
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetAllProductsShouldReturnArrayOfProducts(): void
+    {
+        $this->client = $this->createAuthenticatedClient();
+
+        $this->client->request('GET', '/api/products');
+        $response = $this->client->getResponse();
+
+        self::assertResponseStatusCodeSame(206);
+        self::assertJson($response->getContent());
+        self::assertResponseHasHeader('Content-Type', 'application/hal+json');
 
         $data = json_decode(
-            $client->getResponse()->getContent(),
+            $response->getContent(),
             true,
             512,
             JSON_THROW_ON_ERROR
         );
 
-        $client->setServerParameter('HTTP_Authorization', "Bearer {$data['token']}");
-
-        return $client;
-    }
-
-    public function testGetAllProductsWithoutJWT(): void
-    {
-        $client = self::createClient();
-
-        $client->request('GET', '/api/products');
-        self::assertResponseStatusCodeSame(401);
-    }
-
-    /**
-     * @throws \JsonException
-     */
-    public function testGetAllProducts(): void
-    {
-        $client = $this->createAuthenticatedClient();
-        $serializer = static::getContainer()->get('serializer');
-
-        $client->request('GET', '/api/products');
-        $response = $client->getResponse();
-
-        self::assertResponseIsSuccessful();
-        self::assertJson($response->getContent());
-
-        // Serialize the response content
-        $products = $serializer->deserialize(
-            $response->getContent(),
-            'App\Entity\Product[]',
-            'json'
+        $products = $this->serializer->deserialize(
+            json_encode($data['_embedded'], JSON_THROW_ON_ERROR),
+            'array<App\Entity\Product>',
+            'json',
         );
 
-        // check that for each product, it's instance of Product
         foreach ($products as $product) {
             self::assertInstanceOf(Product::class, $product);
         }
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function createAuthenticatedClient(): KernelBrowser
+    {
+        $this->client->request(
+            'POST',
+            '/api/login',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: '{"email":"pierre.thiollent76@gmail.com","password":"test"}'
+        );
+
+        $data = json_decode(
+            $this->client->getResponse()->getContent(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+
+        $this->client->setServerParameter('HTTP_Authorization', "Bearer {$data['token']}");
+
+        return $this->client;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function testGetAllProductsShouldReturnJsonLdData(): void
+    {
+        $this->client = $this->createAuthenticatedClient();
+
+        $this->client->request('GET', '/api/products');
+        $response = $this->client->getResponse();
+
+        $response = json_decode(
+            $response->getContent(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+
+        self::assertArrayHasKey('page', $response);
+        self::assertArrayHasKey('limit', $response);
+        self::assertArrayHasKey('pages', $response);
+        self::assertArrayHasKey('total', $response);
+
+        self::assertArrayHasKey('_links', $response);
+        self::assertArrayHasKey('self', $response['_links']);
+        self::assertArrayHasKey('href', $response['_links']['self']);
+
+        self::assertArrayHasKey('first', $response['_links']);
+        self::assertArrayHasKey('last', $response['_links']);
+    }
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->serializer = static::getContainer()->get('jms_serializer');
     }
 }
