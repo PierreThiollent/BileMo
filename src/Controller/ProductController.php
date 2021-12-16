a20e8966898294cf919bfd838c2fd04d7104b8b5
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-12-16 17:48:44 +0100

    Add context to serializer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\PaginationService;
+use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,7 +17,7 @@ class ProductController extends AbstractController
 {
     public function __construct(
         private SerializerInterface $serializer,
-        private PaginationService   $pagination,
+        private PaginationService $pagination,
     ) {
     }
 
@@ -25,24 +26,39 @@ class ProductController extends AbstractController
      */
     public function index(ProductRepository $repository, Request $request): JsonResponse
     {
+        $page = $request->query->getInt('page', 1);
+
         $pagination = $this->pagination->paginate(
-            $repository,
+            count($repository->findAll()),
+            $repository->findBy([], limit: 5, offset: $page * 5 - 5),
             'api_get_products',
-            $request->query->getInt('page', 1)
+            $page
         );
 
+        $context = SerializationContext::create()
+            ->setGroups([
+                'Default',
+                'list',
+            ]);
+
         return new JsonResponse(
-            $this->serializer->serialize($pagination, 'json'),
+            $this->serializer->serialize($pagination, 'json', $context),
             Response::HTTP_PARTIAL_CONTENT,
             ['Content-Type' => 'application/hal+json'],
             true
         );
     }
 
-    public function show(Product $product, Request $request): JsonResponse
+    public function show(Product $product): JsonResponse
     {
+        $context = SerializationContext::create()
+            ->setGroups([
+                'Default',
+                'detail',
+            ]);
+
         return new JsonResponse(
-            $this->serializer->serialize($product, 'json'),
+            $this->serializer->serialize($product, 'json', $context),
             Response::HTTP_OK,
             ['Content-Type' => 'application/hal+json'],
             true
