c4f34b5340c209eeaba75115996700cd982e9d5e
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-12-17 10:54:31 +0100

    Add Hateoas annotations to manage JSON LD data.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,7 +5,53 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
+use Hateoas\Configuration\Annotation as Hateoas;
 
+/** @Hateoas\Relation(
+ *    "self",
+ *    href = @Hateoas\Route(
+ *        "api_get_user",
+ *        parameters = {"id" = "expr(object.getId())"},
+ *        absolute = true
+ *    ),
+ * )
+ * @Hateoas\Relation(
+ *     "previous",
+ *     href = @Hateoas\Route(
+ *         "api_get_user",
+ *         parameters = {"id" = "1"},
+ *         absolute = true
+ *     ),
+ *     exclusion = @Hateoas\Exclusion(groups={"detail"}, excludeIf="expr(object.getId() == 1)")
+ * )
+ * @Hateoas\Relation(
+ *     "next",
+ *     href = @Hateoas\Route(
+ *         "api_get_user",
+ *         parameters = {"id" = "expr(object.getId() + 1)"},
+ *         absolute = true
+ *     ),
+ *     exclusion = @Hateoas\Exclusion(groups={"detail"})
+ * )
+ * @Hateoas\Relation(
+ *     "first",
+ *     href = @Hateoas\Route(
+ *         "api_get_user",
+ *         parameters = {"id" = "1"},
+ *         absolute = true
+ *     ),
+ *     exclusion = @Hateoas\Exclusion(groups={"detail"})
+ * )
+ * @Hateoas\Relation(
+ *     "last",
+ *     href = @Hateoas\Route(
+ *         "api_get_user",
+ *         parameters = {"id" = "expr(object.getId())"},
+ *         absolute = true
+ *     ),
+ *     exclusion = @Hateoas\Exclusion(groups={"detail"})
+ * )
+ */
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
 {
